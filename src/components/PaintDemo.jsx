4ab123a3dc2a113068afa9cea62b3cab374256b7
murtaza <muhammadmurtazanasir90@gmail.com>
2024-09-11 16:25:07 +0300

    fi4

--- src/components/PaintDemo.jsx
+++ src/components/PaintDemo.jsx
@@ -10,7 +10,6 @@ import {
   Transformer,
 } from "react-konva";
 import { v4 as uuidv4 } from "uuid";
-import { Arrow, Circle, Rectangle, Scribble } from "./Paint.types";
 import { DrawAction, PAINT_OPTIONS } from "./Paint.constants";
 import { SketchPicker } from "react-color";
 import {
@@ -27,7 +26,8 @@ import {
 } from "@chakra-ui/react";
 import { Download, Upload, XLg } from "react-bootstrap-icons";
 
-const SIZE = 500;
+const SIZE = 550;
+const DEFAULT_BACKGROUND_COLOR = "#fff";
 
 const downloadURI = (uri, name) => {
   const link = document.createElement("a");
@@ -42,7 +42,6 @@ export const PaintDemo = React.memo(function Paint() {
   const currentShapeRef = useRef();
   const isPaintRef = useRef(false);
   const stageRef = useRef(null);
-
   const transformerRef = useRef(null);
 
   const [scribbles, setScribbles] = useState([]);
@@ -52,7 +51,10 @@ export const PaintDemo = React.memo(function Paint() {
   const [image, setImage] = useState();
 
   const [color, setColor] = useState("#000");
-  const [backgroundColor, setBackgroundColor] = useState("#fff"); // Initial background color state
+
+  const [backgroundColor, setBackgroundColor] = useState(
+    DEFAULT_BACKGROUND_COLOR
+  ); // Initial background color state
   const [drawAction, setDrawAction] = useState(DrawAction.Scribble);
   const [isBgColorMode, setIsBgColorMode] = useState(false); // Toggle background color mode
 
@@ -202,8 +204,6 @@ export const PaintDemo = React.memo(function Paint() {
     }
   }, [drawAction]);
 
-  const diagramRef = useRef(null);
-
   const onShapeClick = useCallback(
     (e) => {
       if (drawAction !== DrawAction.Select) return;
@@ -247,176 +247,186 @@ export const PaintDemo = React.memo(function Paint() {
     setImage(undefined);
   }, []);
 
+   const toggleBackgroundColorMode = () => {
+    if (isBgColorMode) {
+      // If background color mode is already on, reset the background color to white
+      setBackgroundColor(DEFAULT_BACKGROUND_COLOR);
+    }
+    setIsBgColorMode((prevMode) => !prevMode); // Toggle mode
+  };
   const onBackgroundColorChange = (color) => {
-    setBackgroundColor(color.hex); // Update background color
-    window.selectedColor = color.hex; // Store globally
+    setBackgroundColor(color.hex); // Update background color when color picker is used
   };
-
-  const toggleBackgroundColorMode = () => {
-    setIsBgColorMode((prevMode) => !prevMode); // Toggle background color mode
-  };
+  const diagramRef = useRef(null);
 
   return (
-    <Box m={4} width={`${SIZE}px`}>
-      <Flex justifyContent={"space-between"} alignItems="center">
-        <ButtonGroup size="sm" isAttached variant="solid">
-          {PAINT_OPTIONS.map(({ id, label, icon }) => (
-            <IconButton
-              key={id}
-              aria-label={label}
-              icon={icon}
-              onClick={() => setDrawAction(id)}
-              colorScheme={id === drawAction ? "whatsapp" : undefined}
-            />
-          ))}
-          <Popover>
-            <PopoverTrigger>
-              <Box
-                bg={color}
-                h={"32px"}
-                w={"32px"}
-                borderRadius="sm"
-                cursor="pointer"
-              ></Box>
-            </PopoverTrigger>
-            <PopoverContent width="300">
-              <PopoverArrow />
-              <PopoverCloseButton />
-              <SketchPicker
-                color={color}
-                onChangeComplete={(selectedColor) =>
-                  setColor(selectedColor.hex)
-                }
-              />
-            </PopoverContent>
-          </Popover>
-          <IconButton aria-label="Upload image" onClick={onImportImageClick}>
-            <Upload />
-          </IconButton>
-          <input
-            ref={fileRef}
-            type="file"
-            style={{ display: "none" }}
-            onChange={onImportImageSelect}
+    <Box m={4}>
+    <Flex justifyContent={"space-between"} alignItems="center">
+      <ButtonGroup size="sm" isAttached variant="solid">
+        {PAINT_OPTIONS.map(({ id, label, icon }) => (
+          <IconButton
+            key={id}
+            aria-label={label}
+            icon={icon}
+            onClick={() => setDrawAction(id)}
+            colorScheme={id === drawAction ? "whatsapp" : undefined}
           />
-        </ButtonGroup>
-        <ButtonGroup>
-          <IconButton onClick={onExportClick} aria-label="Export">
-            <Download />
-          </IconButton>
-          <IconButton onClick={onClear} aria-label="Clear canvas">
-            <XLg />
-          </IconButton>
-          <Popover>
-            <PopoverTrigger>
-              <IconButton
-                id="background-color-icon"
-                aria-label="Toggle Background Color"
-                icon={<Box bg={backgroundColor} h={"32px"} w={"32px"} />}
-              />
-            </PopoverTrigger>
-            <PopoverContent width="200px" >
-              <SketchPicker
-                color={backgroundColor}
-                onChangeComplete={onBackgroundColorChange} // Handle color change
-              />
-            </PopoverContent>
-          </Popover>
-        </ButtonGroup>
-      </Flex>
-      <Box
-        border="1px solid black"
-        mt={4}
-        overflow="hidden"
-        bg={backgroundColor}
+        ))}
+        <Popover>
+          <PopoverTrigger>
+            <Box
+              bg={color}
+              h={"32px"}
+              w={"32px"}
+              borderRadius="sm"
+              cursor="pointer"
+            />
+          </PopoverTrigger>
+          <PopoverContent width="300">
+            <PopoverArrow />
+            <PopoverCloseButton />
+            <SketchPicker
+              color={color}
+              onChangeComplete={(selectedColor) => setColor(selectedColor.hex)}
+            />
+          </PopoverContent>
+        </Popover>
+        <IconButton aria-label="Upload image" onClick={onImportImageClick}>
+          <Upload  size={14} />
+        </IconButton>
+        <input
+          ref={fileRef}
+          type="file"
+          style={{ display: "none" }}
+          onChange={onImportImageSelect}
+        />
+      </ButtonGroup>
+      <ButtonGroup>
+        <IconButton onClick={onExportClick} aria-label="Export" >
+          <Download size={14} />
+        </IconButton>
+        <IconButton onClick={onClear} aria-label="Clear canvas">
+          <XLg size={14} />
+        </IconButton>
+        <Button onClick={toggleBackgroundColorMode} className="small-text">
+            {isBgColorMode ? "Reset Background" : "Change Background"}
+          </Button>
+
+          {/* Show background color icon and color picker only if background color mode is on */}
+          {isBgColorMode && (
+            <Popover>
+              <PopoverTrigger>
+                <IconButton
+                  aria-label="Background color picker"
+                  icon={
+                    <Box bg={backgroundColor} h={"32px"} w={"32px"} />
+                  }
+                />
+              </PopoverTrigger>
+              <PopoverContent width="200px">
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <SketchPicker
+                  color={backgroundColor}
+                  onChangeComplete={onBackgroundColorChange} // Update color
+                />
+              </PopoverContent>
+            </Popover>
+          )}
+      </ButtonGroup>
+    </Flex>
+    <Box
+      border="1px solid black"
+      mt={4}
+      overflow="hidden"
+      bg={backgroundColor} // Set background color
+    >
+      <Stage
+        height={SIZE}
+        width={SIZE}
+        ref={stageRef}
+        onMouseUp={onStageMouseUp}
+        onMouseDown={onStageMouseDown}
+        onMouseMove={onStageMouseMove}
       >
-        <Stage
-          height={SIZE}
-          width={SIZE}
-          ref={stageRef}
-          onMouseUp={onStageMouseUp}
-          onMouseDown={onStageMouseDown}
-          onMouseMove={onStageMouseMove}
-        >
-          <Layer>
-            <KonvaRect
+        <Layer>
+          <KonvaRect
+            x={0}
+            y={0}
+            height={SIZE}
+            width={SIZE}
+            fill={backgroundColor} // Set background color
+            id="bg"
+          />
+          {image && (
+            <KonvaImage
+              ref={diagramRef}
+              image={image}
               x={0}
               y={0}
-              height={SIZE}
-              width={SIZE}
-              fill={backgroundColor} 
-              id="bg"
-              
+              height={SIZE / 2}
+              width={SIZE / 2}
+              onClick={onShapeClick}
+              draggable={isDraggable}
             />
-            {image && (
-              <KonvaImage
-                ref={diagramRef}
-                image={image}
-                x={0}
-                y={0}
-                height={SIZE / 2}
-                width={SIZE / 2}
-                onClick={onShapeClick}
-                draggable={isDraggable}
-              />
-            )}
-            {rectangles?.map((rectangle) => (
-              <KonvaRect
-                key={rectangle.id}
-                x={rectangle?.x}
-                y={rectangle?.y}
-                onClick={onShapeClick}
-                height={rectangle?.height}
-                width={rectangle?.width}
-                stroke={rectangle?.color}
-                id={rectangle?.id}
-                strokeWidth={4}
-                draggable={isDraggable}
-              />
-            ))}
-            {circles?.map((circle) => (
-              <KonvaCircle
-                key={circle.id}
-                id={circle.id}
-                x={circle?.x}
-                y={circle?.y}
-                radius={circle?.radius}
-                stroke={circle?.color}
-                strokeWidth={4}
-                draggable={isDraggable}
-                onClick={onShapeClick}
-              />
-            ))}
-            {scribbles.map((scribble) => (
-              <KonvaLine
-                key={scribble.id}
-                id={scribble.id}
-                lineCap="round"
-                lineJoin="round"
-                stroke={scribble?.color}
-                strokeWidth={4}
-                points={scribble.points}
-                name={DrawAction.Scribble}
-                onClick={onShapeClick}
-                draggable={isDraggable}
-              />
-            ))}
-            {arrows.map((arrow) => (
-              <KonvaArrow
-                key={arrow.id}
-                id={arrow.id}
-                points={arrow.points}
-                fill={arrow.color}
-                stroke={arrow.color}
-                strokeWidth={4}
-                onClick={onShapeClick}
-                draggable={isDraggable}
-              />
-            ))}
-            <Transformer ref={transformerRef} />
-          </Layer>
-        </Stage>
-      </Box>
+          )}
+          {rectangles.map((rectangle) => (
+            <KonvaRect
+              key={rectangle.id}
+              x={rectangle.x}
+              y={rectangle.y}
+              onClick={onShapeClick}
+              height={rectangle.height}
+              width={rectangle.width}
+              stroke={rectangle.color}
+              id={rectangle.id}
+              strokeWidth={4}
+              draggable={isDraggable}
+            />
+          ))}
+          {circles.map((circle) => (
+            <KonvaCircle
+              key={circle.id}
+              id={circle.id}
+              x={circle.x}
+              y={circle.y}
+              radius={circle.radius}
+              stroke={circle.color}
+              strokeWidth={4}
+              draggable={isDraggable}
+              onClick={onShapeClick}
+            />
+          ))}
+          {scribbles.map((scribble) => (
+            <KonvaLine
+              key={scribble.id}
+              id={scribble.id}
+              lineCap="round"
+              lineJoin="round"
+              stroke={scribble.color}
+              strokeWidth={4}
+              points={scribble.points}
+              name={DrawAction.Scribble}
+              onClick={onShapeClick}
+              draggable={isDraggable}
+            />
+          ))}
+          {arrows.map((arrow) => (
+            <KonvaArrow
+              key={arrow.id}
+              id={arrow.id}
+              points={arrow.points}
+              fill={arrow.color}
+              stroke={arrow.color}
+              strokeWidth={4}
+              onClick={onShapeClick}
+              draggable={isDraggable}
+            />
+          ))}
+          <Transformer ref={transformerRef} />
+        </Layer>
+      </Stage>
     </Box>
+  </Box>
   );
 });
